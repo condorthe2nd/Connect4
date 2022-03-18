2777ddb4bfcb00d16ce34681ae5453cfa9408727
Yitzy Holtzberg <yitzyh18@gmail.com>
2022-03-18 10:56:17 -0400

    modified version of board is not getting passed to print

--- Connect Four/task/src/connectfour/Main.kt
+++ Connect Four/task/src/connectfour/Main.kt
@@ -1,5 +1,6 @@
 package connectfour
 
+/* I don't think the modified version of board is getting passed to print*/
 var row = 6
 var column = 7
 
@@ -10,17 +11,19 @@ fun main() {
     val player1 = readln()
     println("Second player's name:")
     val player2 = readln()
-    createBoard(regex, player1, player2)
+    createBoard(regex)
     var board = MutableList(column) { MutableList(row) { ' ' } }
     printBoard(player1, player2, board)
     //println("$row $column")
     while (true) {
-        if (move(player1, 'o', board)) break
-        if (move(player2, '*', board)) break
+        if (move(player1, 'o', board, player1, player2)) break
+        if (move(player2, '*', board, player1, player2)) break
     }
 }
 
-private fun move(player: String, piece: Char, board: MutableList<MutableList<Char>>): Boolean {
+private fun move(
+    player: String, piece: Char, board: MutableList<MutableList<Char>>, player1: String, player2: String
+): Boolean {
     var a = false
     println("$player's turn")
     while (true) {
@@ -38,7 +41,7 @@ private fun move(player: String, piece: Char, board: MutableList<MutableList<Cha
             println("The column is full")
             continue
         } else {
-            for (i in 0 until board.size) {
+            for (i in 0..board.size) {
                 if (board[i][board[i].lastIndex] == ' ') {
                     if (board[number - 1][i] == ' ') {
                         board[number - 1][i] = piece
@@ -46,6 +49,7 @@ private fun move(player: String, piece: Char, board: MutableList<MutableList<Cha
                     }
                 }
             }
+            printBoard(player1, player2, board)
             println(board)
         }
         break
@@ -53,18 +57,20 @@ private fun move(player: String, piece: Char, board: MutableList<MutableList<Cha
     return a
 }
 
-private fun createBoard(regex: Regex, player1: String, player2: String) {
+private fun createBoard(regex: Regex) {
     while (true) {
         println("Set the board dimensions (Rows x Columns)")
         println("Press Enter for default (6 x 7)")
         val line = readln()
         if (line.isEmpty()) {
+
         } else if (line == "end") {
             break
         } else {
             val new = line.replace("\\s".toRegex(), "").toCharArray()
             if (new.size > 3) {
-                val splitNumbers = line.replace("\\s".toRegex(), "").split("x")
+                val splitNumbers = line.replace("\\s".toRegex(), "")
+                    .split("x")
                 if (!regex.matches(new.joinToString(""))) {
                     print("Invalid input\n")
                     continue
@@ -96,28 +102,32 @@ private fun createBoard(regex: Regex, player1: String, player2: String) {
     }
 }
 
-private fun printBoard(name1: String, name2: String, board: MutableList<MutableList<Char>>) {
+private fun printBoard(
+    name1: String, name2: String, board: MutableList<MutableList<Char>>
+) {
     println("$name1 VS $name2")
     println("$row X $column board")
     var count = 0
     print(" ")
     repeat(column) {
         count++
-        print("$count ")
+        print("$count  ")
     }
-    for (i in 1..row) {
+    for (i in 0..row) {
         println()
         for (j in 0..column) {
-            print("║ ")
-            if (board[i][j] != ' ') {
-                print(board[i][j])
+
+            if (board[i].size < i && board[i][j] != ' ') {
+                print("║${board[i][j]}* ")
+            } else {
+                print("║  ")
             }
         }
     }
     print("\n╚═")
     repeat(column - 1) {
-        print("╩═")
+        print("═╩═")
     }
-    print("╝\n")
+    print("═╝\n")
 }
 
